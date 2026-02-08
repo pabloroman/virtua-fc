6cdf81ed48a3ac5cbcefbeb56276ef879aa403a3
Pablo Román <pablo.roman85@gmail.com>
2026-02-08 10:14:42 +0100

    Load telescope condiionally

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,10 @@
 
 namespace App\Providers;
 
-use App\Domain\Competition\CompetitionRepository;
 use App\Game\Handlers\KnockoutCupHandler;
 use App\Game\Handlers\LeagueHandler;
 use App\Game\Handlers\LeagueWithPlayoffHandler;
 use App\Game\Services\CompetitionHandlerResolver;
-use App\Infrastructure\Repositories\DatabaseCompetitionRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,12 +16,8 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
-            $this->app->register(\App\Providers\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
         }
-
-        $this->app->bind(CompetitionRepository::class, function () {
-            return new DatabaseCompetitionRepository();
-        });
 
         // Register competition handler resolver as singleton
         $this->app->singleton(CompetitionHandlerResolver::class, function ($app) {
