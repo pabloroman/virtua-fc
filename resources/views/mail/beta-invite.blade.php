804566278ff4efdffed64877e39d0b97a4c3a11c
Pablo Román <pablo.roman85@gmail.com>
2026-02-11 22:09:09 +0100

    Plain text emails

--- resources/views/mail/beta-invite.blade.php
+++ resources/views/mail/beta-invite.blade.php
@@ -1,16 +1,12 @@
-<x-mail::message>
-# {{ __('beta.email_greeting') }}
+{{ __('beta.email_greeting') }}
 
 {{ __('beta.email_body') }}
 
-<x-mail::button :url="$registerUrl">
-{{ __('beta.email_cta') }}
-</x-mail::button>
+{{ $registerUrl }}
 
-{{ __('beta.email_code_label') }} **{{ $code }}**
+{{ __('beta.email_code_label') }} {{ $code }}
 
 {{ __('beta.email_warning') }}
 
-{{ __('beta.email_thanks') }}<br>
-{{ config('app.name') }}
-</x-mail::message>
+{{ __('beta.email_thanks') }}
+Pablo — {{ config('app.name') }}
